51d01fcdbe90857b78d37f893225cb7f103454cc
antipsycho <antipsycho@reddit.com>
2011-11-13 01:06:21 +0000

    Basic data gathering from reddit, and item path hierarchy

--- idx/gather.py
+++ idx/gather.py
@@ -1,31 +1,80 @@
 from datetime import datetime
+import logging
 import pytz
 import re
 
 import reddit
 import solr
 
+logging.basicConfig(filename='logs/gather.log', level=logging.INFO)
+
+solr = solr.SolrConnection('http://localhost:8983/solr')
 reddit = reddit.Reddit(user_agent='occupymanifesto')
 subreddit = reddit.get_subreddit('occupymanifesto')
 
 nodes = []
 for item in subreddit.get_hot(limit=None):
 
-	matches = re.match('(.*):', item.title)
+	logging.info('Processing {%s}' % item)
 
-	if not matches or not matches.group(1) in ('Principle', 'Rationale', 'Evidence'):
+	# Only process submissions which have a valid noun-phrase type
+	type = None
+	matches = re.match('(.*):', item.title)
+	if not matches:
+		logging.info('Skipping {%s} since it has no noun-phrase type specified' % item)
 		continue
 
+	# Only text submissions are currently considered for visualization
+	if not item.__dict__.has_key('selftext'):
+		logging.info('Skipping {%s} since it does not contain a selftext field' % item)
+		continue
+
+	type = matches.group(1).strip()
+
+	# Link this item to any existing parent
+	parent = None
+	parent_path = None
+	for line in item.selftext.splitlines():
+
+		# Scan the message line-by-line looking for parent identifiers
+		matches = re.match('parent:(.*)', line)
+		if not matches:
+			continue
+
+		# Extract the parent item ID
+		parent_url = matches.group(1).strip()
+		matches = re.match('(http.?://.*)(/r/)(.*)(/comments/)(.*)(/.*/)', parent_url)
+
+		if not matches:
+			continue
+
+		# Extract the parent document's full path
+		parent = matches.group(5)
+		parent_path = None
+		response = solr.query('id:%s' % parent)
+		if response.results and response.results[0].has_key('path'):
+			parent_path = response.results[0]['path'] 
+			break
+
+	# Build up this item's path
+	path = (parent_path or '') + '/' + item.id
+	logging.info('Constructed path %s for {%s}' % (path, item))
+
+	# Prepare a document to be loaded into Solr
 	node = {
-		'id': item.id,
-		'type': matches.group(1),
-		'title': item.title.encode('utf-8'),
 		'datetime': datetime.fromtimestamp(item.created_utc, pytz.UTC),
-		'points': item.score
+
+		'id': item.id,
+		'type': type,
+		'path': path,
+		'parent': parent,
+
+		'points': item.score,
+		'title': item.title.encode('utf-8')
 	}
 	nodes.append(node)
 
-solr = solr.SolrConnection('http://localhost:8983/solr')
+# Load documents into Solr and commit the changes
 solr.add_many(nodes)
 solr.commit()
 solr.close()
