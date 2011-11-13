3b23fa75e70bc9eecc7e8a960384001058c07f17
antipsycho <antipsycho@reddit.com>
2011-11-13 18:11:13 +0000

    Text layout improvements

--- www/js/ajax-solr/widgets/jquery/GraphWidget.js
+++ www/js/ajax-solr/widgets/jquery/GraphWidget.js
@@ -7,9 +7,13 @@ AjaxSolr.GraphWidget = AjaxSolr.AbstractWidget.extend({
     renderer: null,
 
     init: function() {
+
+        var width = $('#' + this.target).width();
+        var height = $('#' + this.target).height();
+
         this.graph = new Graph();
         this.layouter = new Graph.Layout.Spring(this.graph);
-        this.renderer = new Graph.Renderer.Raphael(this.target, this.graph);
+        this.renderer = new Graph.Renderer.Raphael(this.target, this.graph, width, height);
     },
 
     redraw: function() {
@@ -18,12 +22,29 @@ AjaxSolr.GraphWidget = AjaxSolr.AbstractWidget.extend({
     },
 
     afterRequest: function () {
+
+        $('#' + self.target).empty();
+
+        // First pass - add all documents as nodes
+        for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+            var doc = this.manager.response.response.docs[i];
+            var title = doc.title;
+
+            if (title.length > 24) {
+                title = title.substring(0, 23) + '...';
+            }
+
+            this.graph.addNode(doc.id, {label: title});
+        }
+
+        // Second pass - draw directed edges between nodes
         for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
             var doc = this.manager.response.response.docs[i];
             if (!doc.parent) continue;
-
-            this.graph.addEdge(doc.id, doc.parent, {'directed': true});
+            this.graph.addEdge(doc.id, doc.parent, {directed: true});
         }
+
+        // Render the diagram
         this.redraw();
     }
 });
